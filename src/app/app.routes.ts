5424aa7682f3279c386cf71fbcf042c47111c265
gweston@aictitle.com <gweston@aictitle.com>
2024-06-27 11:58:06 -0500

    brought more had to increase budget

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -75,7 +75,67 @@ export const appRoutes: Route[] = [
             initialData: initialDataResolver
         },
         children: [
+            {path: 'ad-search-customer-show', loadChildren: () => import('app/modules/admin/ad-search/ad-search.routes')},
+            {path: 'ad-hc-censusresults', loadChildren: () => import('app/modules/admin/ad-healthcensus/ad-healthcensus.routes')},
+            {path: 'ad-hc-employeecensus', loadChildren: () => import('app/modules/admin/ad-healthcensus/ad-healthcensus.routes')},
+            {path: 'ad-stdc-bootstrap', loadChildren: () => import('app/modules/admin/ad-sampletemplate/ad-sampletemplate.routes')},
+            {path: 'ad-stdc-fontawesome', loadChildren: () => import('app/modules/admin/ad-sampletemplate/ad-sampletemplate.routes')},
+            {path: 'ad-stdc-googlefonts', loadChildren: () => import('app/modules/admin/ad-sampletemplate/ad-sampletemplate.routes')},
+            {path: 'ad-stdc-latestversion', loadChildren: () => import('app/modules/admin/ad-sampletemplate/ad-sampletemplate.routes')},
+            {path: 'ad-stdc-more', loadChildren: () => import('app/modules/admin/ad-sampletemplate/ad-sampletemplate.routes')},
+            
+            {path: 'ad-virtual-acrprodtest', loadChildren: () => import('app/modules/admin/ad-virtual/ad-virtual.routes')},
+            {path: 'ad-virtual-acrproduction', loadChildren: () => import('app/modules/admin/ad-virtual/ad-virtual.routes')},
+            {path: 'ad-virtual-aicvirtualclosing', loadChildren: () => import('app/modules/admin/ad-virtual/ad-virtual.routes')},
+            {path: 'ad-virtual-aicvirtualclosingprodtest', loadChildren: () => import('app/modules/admin/ad-virtual/ad-virtual.routes')},
+            {path: 'ad-virtual-dev', loadChildren: () => import('app/modules/admin/ad-virtual/ad-virtual.routes')},
+            {path: 'ad-virtual-global', loadChildren: () => import('app/modules/admin/ad-virtual/ad-virtual.routes')},
+            {path: 'ad-virtual-globalprodtest', loadChildren: () => import('app/modules/admin/ad-virtual/ad-virtual.routes')},
+            {path: 'ad-virtual-myglobalvirtual', loadChildren: () => import('app/modules/admin/ad-virtual/ad-virtual.routes')},
+            {path: 'ad-virtual-mytextronvirtual', loadChildren: () => import('app/modules/admin/ad-virtual/ad-virtual.routes')},
+            {path: 'ad-virtual-myvirtualclosing', loadChildren: () => import('app/modules/admin/ad-virtual/ad-virtual.routes')},
+            {path: 'ad-virtual-sandbox', loadChildren: () => import('app/modules/admin/ad-virtual/ad-virtual.routes')},            
+            
+            {path: 'ad-admin-manageusers', loadChildren: () => import('app/modules/admin/ad-admin/ad-admin.routes')},
+            {path: 'ad-admin-roles', loadChildren: () => import('app/modules/admin/ad-admin/ad-admin.routes')},
+            {path: 'ad-admin-rpsqueue', loadChildren: () => import('app/modules/admin/ad-admin/ad-admin.routes')},
+            {path: 'ad-admin-userlist', loadChildren: () => import('app/modules/admin/ad-admin/ad-admin.routes')},
+            
+            {path: 'ad-manage-onespan', loadChildren: () => import('app/modules/admin/ad-manage/ad-manage.routes')},
+            
+            {path: 'ad-accounting-accountingstatement', loadChildren: () => import('app/modules/admin/ad-accounting/ad-accounting.routes')},
+            {path: 'ad-accounting-closingstatement', loadChildren: () => import('app/modules/admin/ad-accounting/ad-accounting.routes')},
+            
+            {path: 'ad-search-orders', loadChildren: () => import('app/modules/admin/ad-search/ad-search.routes')},
+            {path: 'ad-search-customers', loadChildren: () => import('app/modules/admin/ad-search/ad-search.routes')},
+            
+            {path: 'ad-closingroom/ad-cr-test1', loadChildren: () => import('app/modules/admin/ad-closingroom/ad-closingroom.routes')},
+            
+            {path: 'ad-cr-search', loadChildren: () => import('app/modules/admin/ad-closingroom/ad-closingroom.routes')},
+            {path: 'ad-cr-all', loadChildren: () => import('app/modules/admin/ad-closingroom/ad-closingroom.routes')},
+            {path: 'ad-cr-open', loadChildren: () => import('app/modules/admin/ad-closingroom/ad-closingroom.routes')},
+            {path: 'ad-cr-closed', loadChildren: () => import('app/modules/admin/ad-closingroom/ad-closingroom.routes')},
+            
+            {path: 'ad-closingroom', loadChildren: () => import('app/modules/admin/ad-closingroom/ad-closingroom.routes')},
+            
+            {path: 'ad-home', loadChildren: () => import('app/modules/admin/ad-home/ad-home.routes')},
+            
             {path: 'example', loadChildren: () => import('app/modules/admin/example/example.routes')},
         ]
+    },
+
+    {
+        path: 'ad-eodetail/:escrowOrderID',
+        canActivate: [AuthGuard],
+        canActivateChild: [AuthGuard],
+        component: ADEODetailComponent,
+        resolve: {
+            initialData: initialDataResolver,
+            loadChildren: () => import('app/modules/admin/ad-eodetail/ad-eodetail.routes')
+        },
+        children: [
+            
+        ]
     }
+
 ];
