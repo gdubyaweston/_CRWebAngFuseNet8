464441cdc16c81c30439939bf72c7ca184812c03
gweston@aictitle.com <gweston@aictitle.com>
2024-06-28 09:56:41 -0500

    del

--- src/app/modules/admin/ad-home/ad-home.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { ADHomeComponent } from 'app/modules/admin/ad-home/ad-home.component';
-
-export default [
-    {
-        path     : '',
-        component: ADHomeComponent,
-    },
-] as Routes;
--- src/app/modules/admin/ad-search/ad-search.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { ADSearchComponent } from 'app/modules/admin/ad-search/ad-search.component';
-
-export default [
-    {
-        path     : '',
-        component: ADSearchComponent,
-    },    
-] as Routes;
